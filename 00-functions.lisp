299609c18c0c65c5eae8c3011f33dc8e4a038685
Tyler Lee <wtleeiv@gmail.com>
2017-10-24 23:35:28 -0700

    more things

--- 00-functions.lisp
+++ 00-functions.lisp
@@ -19,3 +19,43 @@
 (funcall (eval a) 4) ; 8
 ;; apply ~ apply fn object to arg list
 (apply (eval a) '(4)) ; 8
+
+(defun ty-remove-if (fn lst)
+  (if (null lst)
+      nil
+      (if (funcall fn (car lst))
+          (ty-remove-if fn (cdr lst))
+          (cons (car lst) (ty-remove-if fn (cdr lst))))))
+
+(let ((counter 0))
+  (defun new-id () (incf counter))
+  (defun reset-count () (setf counter 0)))
+
+(defun make-adder (n)
+  (lambda (x) (+ x n)))
+
+(defun make-two-adder (n)
+  (lambda (x y) (+ x y n)))
+
+(defmacro fun-call (fn &rest params)
+  `(funcall (eval ,fn) ,@params))
+
+(defun slow-count (n)
+  (labels ((iter (n c)
+             (if (eq n 0)
+                 c
+                 (iter
+                  (1- n)
+                  (+ n c)))))
+    (iter n 0)))
+
+(proclaim '(optimize speed)) ; tail-call recursion
+(defun fast-count (n)
+  (labels ((iter (n c)
+             (declare (type fixnum n c))
+             (if (eq n 0)
+                 c
+                 (iter
+                  (the fixnum (1- n))
+                  (the fixnum (+ n c))))))
+    (iter n 0)))
